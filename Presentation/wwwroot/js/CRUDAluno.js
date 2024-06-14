98fe81b58e1ddf50165796f2f20a6770d772af8c
Allandpn <99135710+Allandpn@users.noreply.github.com>
2024-06-13 21:39:17 -0300

    incluido funcao editar aluno

--- Presentation/wwwroot/js/CRUDAluno.js
+++ Presentation/wwwroot/js/CRUDAluno.js
@@ -33,6 +33,55 @@ const formatDate = function(data) {
 }
 
 
+// Função para preencher o formulário com os dados do aluno
+function fillForm(aluno) {
+    document.getElementById('formEditNome').value = aluno.nome;
+    document.getElementById('formEditCPF').value = aluno.cpfCnpj;
+    document.getElementById('formEditDataNasc').value = formatDate(aluno.dataNascimento);
+    document.getElementById('formEditTelefone').value = aluno.telefone;
+    document.getElementById('formEditEmail').value = aluno.email;
+    document.getElementById('formEditEndereco').value = aluno.endereco;
+    document.getElementById('formEditCurso').value = aluno.curso;
+    document.getElementById('formEditTurma').value = aluno.turma;
+}
+
+// Adicionar evento de clique aos botões de edição após a busca
+function adiconaEventoEditar() {    
+    const editButtons = document.querySelectorAll('.btnEditAluno');
+    console.log('Botões de edição encontrados:', editButtons.length);
+
+    editButtons.forEach(function(editBtn) {
+        editBtn.addEventListener('click', function(event) {
+            console.log("Edit button clicked");
+            const row = editBtn.closest('tr');
+            const aluno = {
+                matricula: row.cells[0].textContent.trim(),
+            };
+
+            console.log("Matrícula do aluno:", aluno.matricula);
+
+            fetch(`https://localhost:5000/api/Aluno/${aluno.matricula}`, {
+                method: 'GET',
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao obter os dados do aluno.');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // console.log("Dados do aluno:", data);
+                fillForm(data); // Preencher o formulário com os dados recebidos
+                document.getElementById('updateAlunoBtn').setAttribute('data-matricula', aluno.matricula);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        });
+    });
+}
+
+
 //Retorna todos os registros
 async function buscarAlunosTodos() {
     const url = `https://localhost:5000/api/Aluno/alunos`; // Replace with your API URL
@@ -45,16 +94,18 @@ async function buscarAlunosTodos() {
             date = formatDate(e.dataNascimento)
             contentLine += `
             <tr >
+                <td>${e.matricula}</td>  
                 <td>${e.nome}</td>
                 <td>${date}</td>
                 <td>${e.telefone}</td>
                 <td>${e.email}</td>
-                <td><a href="" data-bs-toggle="modal" data-bs-target="#formEditAluno"><box-icon name='edit'></box-icon></a></td>
+                <td><a class="btnEditAluno" href="" data-bs-toggle="modal" data-bs-target="#formEditAluno"><box-icon name='edit'></box-icon></a></td>
                 <td><a href="" data-bs-toggle="modal" data-bs-target="#apagarAluno"><box-icon type='solid' name='user-x'></box-icon></a></td>
             </tr>
             `        
         });
         content.innerHTML = contentLine
+        adiconaEventoEditar()
     }      
 }
 
@@ -62,10 +113,147 @@ async function buscarAlunosTodos() {
 buscarAlunosTodos()
 
 
+//create Aluno
+document.getElementById('createAlunoBtn').addEventListener('click', function() {
+    function formatDate(dateStr) {
+        const [day, month, year] = dateStr.split('/');
+        const date = new Date(`${year}-${month}-${day}`);
+        return date.toISOString();
+    }
+
+    const nome = document.getElementById('formAddNome').value;
+    const cpfCnpj = document.getElementById('formAddCPF').value;
+    const dataNascimento = document.getElementById('formAddDataNasc').value;
+    const telefone = document.getElementById('formAddTelefone').value;
+    const email = document.getElementById('formAddEmail').value;
+    const endereco = document.getElementById('formAddEndereco').value;
+    const curso = document.getElementById('formAddCurso').value;
+    const turma = document.getElementById('formAddTurma').value;
+
+    const aluno = {
+        nome,
+        cpfCnpj,
+        dataNascimento: formatDate(dataNascimento),
+        telefone,
+        email,
+        endereco,
+        curso,
+        turma
+    };
+
+    fetch('https://localhost:5000/api/Aluno/cadastrar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(aluno)
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(error => {
+                throw new Error(error.message || 'Erro ao cadastrar aluno.');
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        const message = data.message || 'Aluno cadastrado com sucesso!';
+        buscarAlunosTodos()
+        showFlashMessage(message, 'success');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        showFlashMessage(error.message, 'danger');
+    });
+});
+
+function showFlashMessage(message, type) {
+    const flashMessageElement = document.getElementById('flashMessage');
+    flashMessageElement.textContent = message;
+    flashMessageElement.className = `alert alert-${type}`;
+    
+    const flashModal = new bootstrap.Modal(document.getElementById('flashModal'));
+    flashModal.show();
+}
 
 
 
 
+
+//editar Aluno
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM completamente carregado e analisado');
+
+    // Função para formatar data
+    function formatDate(dateStr) {
+        const [day, month, year] = dateStr.split('/');
+        if (day && month && year) {
+            const date = new Date(`${year}-${month}-${day}T00:00:00Z`);
+            return date.toISOString();
+        } else {
+            throw new Error('Data inválida');
+        }
+    }
+
+    
+
+    
+
+    // Adicionar evento de clique ao botão de salvar
+    document.getElementById('updateAlunoBtn').addEventListener('click', function() {
+        const matricula = this.getAttribute('data-matricula');
+        console.log(matricula)
+        const aluno = {
+            nome: document.getElementById('formEditNome').value,
+            cpfCnpj: document.getElementById('formEditCPF').value,
+            dataNascimento: formatDate(document.getElementById('formEditDataNasc').value),
+            telefone: document.getElementById('formEditTelefone').value,
+            email: document.getElementById('formEditEmail').value,
+            endereco: document.getElementById('formEditEndereco').value,
+            curso: document.getElementById('formEditCurso').value,
+            turma: document.getElementById('formEditTurma').value
+        };
+        console.log(aluno)
+
+        fetch(`https://localhost:5000/api/Aluno/atualizar/${matricula}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(aluno)
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(error => {
+                    throw new Error(error.message || 'Erro ao atualizar aluno.');
+                });
+            }           
+        })
+        .then(data => {
+            const message = 'Aluno atualizado com sucesso!';
+            showFlashMessage(message, 'success');
+            document.querySelector('[data-bs-dismiss="modal"]').click(); // Fecha o modal de edição
+            buscarAlunosTodos()
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            showFlashMessage(error.message, 'danger');
+        });
+    });
+
+    // Função para mostrar mensagem flash
+    function showFlashMessage(message, type) {
+        const flashMessageElement = document.getElementById('flashMessage');
+        flashMessageElement.textContent = message;
+        flashMessageElement.className = `alert alert-${type}`;
+        
+        const flashModal = new bootstrap.Modal(document.getElementById('flashModal'));
+        flashModal.show();
+    }
+});
+
+
+//Buscar Aluno pelo Nome
 
 
 
